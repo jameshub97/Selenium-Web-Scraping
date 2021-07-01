1128de697ed4056356bc8028b992e560357c9b89
jameshub97 <jamesalexcarr@outlook.com>
2021-07-01 13:27:18 +0100

    creating tags

--- main.py
+++ main.py
@@ -6,39 +6,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+
+# Init of webdriver
 driver = webdriver.Chrome(ChromeDriverManager().install())
 chrome_options = webdriver.ChromeOptions(); 
 chrome_options.add_experimental_option("excludeSwitches", ['enable-logging']);
 
-# Chhoosing website
+# Website to scrape
 driver.get("https://en.wikipedia.org/wiki/Snake")
 print("Welcome to:", driver.title)
 
-# Locating website elements examples
-# tag = driver.find_element_by_tag_name('h1')
-# element = driver.find_element_by_id("content")
-# print("Finding Tags:", "\n", tag)
-# print("Finding Elements:", "\n", element)
 
-# <input type="search" name="search" placeholder="Search Wikipedia" autocapitalize="sentences" title="Search Wikipedia [alt-shift-f]" accesskey="f" id="searchInput" autocomplete="off">
+# search = driver.find_element_by_name("search")
+# search.send_keys("snake")
+# search.send_keys(Keys.RETURN)
 
-search = driver.find_element_by_name("search")
-search.send_keys("snake")
-search.send_keys(Keys.RETURN)
+# stuff = driver.find_element_by_id("firstHeading")
+# stuff2 = driver.find_element_by_class_name("firstHeading")
+# stuff3 = driver.find_element_by_name("firstHeading")
 
-try: 
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "content"))
-    )
-    articles = main.find_elements_by_class_name('mw-search-result')
-    for article in articles:
-        header = article.find_element_by_class_name("mw-search-result-heading")
-        print(header.text)
-finally:
-    driver.quit()
+tags = driver.find_element_by_tag_name("h1")
+
+# ("/html/body/form[1]")
+print("EXAMPLE", tags.text)
+# print("EXAMPLE", stuff2)
+# print("EXAMPLE", stuff3)
+# for text in texts:
+#     print(texts.text)
 
 
-# time.sleep(5)
 
-# driver.quit()
+# try: 
+#     main = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.ID, "content"))
+#     )
+#     articles = main.find_elements_by_class_name('mw-search-result')
+#     for article in articles:
+#         header = article.find_element_by_class_name("mw-search-result-heading")
+#         print(header.text)
+# finally:
+#     driver.quit()
 
